ac2ba7803a5531968049daceaf268e6f32caf74a
Chenath Perera <chenathperera@gmail.com>
2026-03-26 20:51:48 +0530

    server.js + app.js

---
+++ services/doctor-service/src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import doctorAuthRoutes from "./routes/doctorAuthRoutes.js";
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Health route
+app.get("/", (req, res) => {
+  res.json({
+    service: "doctor-service",
+    status: "running"
+  });
+});
+
+// Doctor auth routes
+app.use("/api/doctors/auth", doctorAuthRoutes);
+
+export default app;
